e4fae317fbc278d7831defc4e72638fd005b536c
Matt <carlotta.matt@gmail.com>
2021-06-24 10:20:45 -0700

    [tests]: Refactor tests for 100% coverage

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,4 +1,4 @@
-import decrypt from "../decrypt";
+import env from "../index";
 
 const envString = {
   ABC: "123",
@@ -10,7 +10,7 @@ const envBuffer = Buffer.from(`ABC=123\nDEF=678\nHIJ=$ABC$DEF`);
 
 describe("Decrypt Method", () => {
   it("decrypts an encrypted JSON string and returns 'decryptedEnvs' and 'decryptedResult'", () => {
-    const { decryptedEnvs, decryptedResult } = decrypt({
+    const { decryptedEnvs, decryptedResult } = env.decrypt({
       algorithm: "aes-256-cbc",
       envs: "d4b6baef6ae9313a17b3f736a4e28ba35f4f23a74397a06f75fefe7acc777b81570a12ccee82ff4e2c05f148dce3b17c",
       encoding: "utf8",
@@ -24,7 +24,7 @@ describe("Decrypt Method", () => {
   });
 
   it("decrypts an encrypted Buffer string and returns 'decryptedEnvs' and 'decryptedResult'", () => {
-    const { decryptedEnvs, decryptedResult } = decrypt({
+    const { decryptedEnvs, decryptedResult } = env.decrypt({
       algorithm: "aes-256-cbc",
       envs: "025d83b7518e7b63b3c2394c412deaecb07f23551f17a40fca415d39da0e5333",
       encoding: "utf8",
@@ -35,5 +35,26 @@ describe("Decrypt Method", () => {
 
     expect(decryptedEnvs).toEqual("ABC=123\nDEF=678\nHIJ=$ABC$DEF");
     expect(decryptedResult).toEqual(envBuffer);
+  });
+});
+
+describe("Encrypt Method", () => {
+  it("encrypts a stringified JSON object and returns an 'encryptedEvs' and 'iv'", () => {
+    const envString = JSON.stringify({
+      ABC: "123",
+      DEF: "678",
+      HIJ: "$ABC$DEF"
+    });
+
+    const { encryptedEvs, iv } = env.encrypt({
+      algorithm: "aes-256-cbc",
+      envs: envString,
+      encoding: "utf8",
+      input: "hex",
+      secret: "abcdefghijklmnopqrstuv1234567890"
+    });
+
+    expect(encryptedEvs).toEqual(expect.any(String));
+    expect(iv).toEqual(expect.any(String));
   });
 });
